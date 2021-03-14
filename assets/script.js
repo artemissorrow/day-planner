7c45a35eeb6e27bae94ecaf757b93a3b80e0bd68
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-03-14 19:34:26 -0400

    successfully saving to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 var currentDay = document.getElementById("currentDay");
-var currentTime = moment().format("h a");
-
+var currentTime = moment().format("ha");
 currentDay.textContent = moment().format("dddd, MMMM Do YYYY");
 
 var scheduleTime = (document.getElementsByClassName("hour"));
+var startTime = moment().hour(8);
 
-for (i = 0; i < scheduleTime.length; i++) {
-    if (currentTime < scheduleTime[i].innerHTML && scheduleTime[i] != scheduleTime[0]) {
+for (h = 0; h < scheduleTime.length; h++) {
+    scheduleTime[h].textContent = startTime.add(1, "hour").format("ha");
+}
+
+for (i = 1; i < scheduleTime.length; i++) {
+    if (currentTime < scheduleTime[i].innerHTML && currentTime) {
         scheduleTime[i].nextElementSibling.className = "future col-10";
     } else if (currentTime === scheduleTime[i].innerHTML) {
         scheduleTime[i].nextElementSibling.className = "present col-10";
@@ -15,25 +19,45 @@ for (i = 0; i < scheduleTime.length; i++) {
     }
 }
 
-var inputData = [];
+$(".saveBtn").on("click", function (e) {
+    var target = $(e.target);
+    var parent = target.parents(".time-block");
+    var textInput = parent.find("textarea");
+    var timePar = parent.find(".hour")
+    var textValue = textInput.val();
+    var time = timePar.text();
 
-$("#parent").click(function (e) {
-    if (e.target.matches(".fa-save")) {
-        console.log("success");
+    // console.log(textValue);
+    // console.log(time);
 
-        var timeBlock = $(e.target).parent().siblings();
-        var hour = $(timeBlock[0].textContent);
-        var textInput = $(timeBlock[1]).children();
-        var textValue = $(textInput.val());
-        console.log(textValue);
+    localStorage.setItem(time, textValue);
 
-    //     var newAppt = {time: hour, appt: textValue};
+});
 
-    //     inputData.push(newAppt);
+function populate() {
+    var keys = Object.keys(localStorage);
+    var timeBlocks = $(".hour");
+    var row = timeBlocks.parent();
+    var textBox = row.find("textarea")
 
-    //     localStorage.setItem("appts", JSON.stringify(inputData));
-    // }
+    var savedTime = []
+    for (var k = 0; k < timeBlocks.length; k++) {
+        savedTime.push(timeBlocks[k].textContent);
     }
-})
+
+    console.log(keys);
+    console.log(savedTime);
+    console.log(textBox);
+
+    for (var j = 0; j < savedTime.length; j++) {
+        if (savedTime[j] == keys[j]) {
+            $(textBox).val("hello world");
+        }
+
+    }
+}
+
+populate();
 
 
+// localStorage.getItem(keys[j]
